2b2eb26ad7d9326e5168a64b10737a15cf5bdaab
Adrian Mowrey <92110733+mowrey@users.noreply.github.com>
2024-06-29 19:57:52 -0400

    Update gemini_native_tts.js
    ..awaits for the answer to complete, then it starts reading

--- gemini_native_tts.js
+++ gemini_native_tts.js
@@ -1,26 +1,26 @@
 let lastButtonElement = null;
 
-function clickLastButton() {
-    const buttonElements = document.querySelectorAll('.mat-mdc-tooltip-trigger.tts-button.mdc-icon-button.mat-mdc-icon-button.mat-unthemed.mat-mdc-button-base.gmat-mdc-button.ng-star-inserted');
-    const latestButtonElement = buttonElements[buttonElements.length - 1];
-    
-    if (latestButtonElement && latestButtonElement !== lastButtonElement) {
-        latestButtonElement.click();
-        lastButtonElement = latestButtonElement;
-    }
+async function clickLastButton() {
+  const buttonElements = document.querySelectorAll('.mat-mdc-tooltip-trigger.tts-button.mdc-icon-button.mat-mdc-icon-button.mat-unthemed.mat-mdc-button-base.gmat-mdc-button.ng-star-inserted');
+  const latestButtonElement = buttonElements[buttonElements.length - 1];
+
+  if (latestButtonElement && latestButtonElement !== lastButtonElement) {
+    await latestButtonElement.click(); // Use await for click action to finish
+    lastButtonElement = latestButtonElement;
+  }
 }
 
 function observeMutations() {
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            clickLastButton();
-        });
-    });
+  const observer = new MutationObserver(async function(mutations) {
+    for (const mutation of mutations) { // Loop through mutations
+      await clickLastButton(); // Wait for each click to finish
+    }
+  });
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
 }
 
 observeMutations();
